d5468a3f05b00f41d6d45525d44bb79449cd5949
Hugo Nebreda <hugo.nebreda@gmail.com>
2017-03-08 20:57:47 +0100

    Change to ES6 syntax

--- session-one/app.js
+++ session-one/app.js
@@ -1,14 +1,8 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World from get!')
-})
+app.get('/', (req, res) => res.send('Hello World from get with ES6!'));
 
-app.post('/', function (req, res) {
-  res.send('Hello World form post!')
-})
+app.post('/', (req, res) => res.send('Hello World form post with ES6!'));
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+app.listen(3000, () =>  console.log('Example app listening on port 3000!'));
