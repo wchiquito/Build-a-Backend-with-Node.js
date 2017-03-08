f7753184e0bd844f57bb10098d1ab6c67e5d8f4d
William Chiquito <wchqiuito@gmail.com>
2017-03-08 19:43:38 +0000

    Add post to Express

--- session-one/app.js
+++ session-one/app.js
@@ -2,7 +2,11 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello World from get!')
+})
+
+app.post('/', function (req, res) {
+  res.send('Hello World form post!')
 })
 
 app.listen(3000, function () {
