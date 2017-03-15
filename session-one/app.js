7b57e249343ffe73723f2eef8318e09b847a1c0c
Hugo Nebreda <hugo.nebreda@gmail.com>
2017-03-15 19:34:05 +0100

    Create array of events

--- session-one/app.js
+++ session-one/app.js
@@ -8,10 +8,36 @@ app.post('/', (req, res) => res.send('Hello World form post with ES6!'));
 app.listen(3000, () =>  console.log('Example app listening on port 3000!'));
 
 
-let event = {
+let events = [
+    {
     id: 1,
     title: "Concierto Metallica",
     description: "Evento Musical de calidad",
     date: "2017-04-09"
-};
+    },
+    {
+        id: 2,
+        title: "Concierto Red Hot Chilli Peppers",
+        description: "Evento Musical de calidad maxima",
+        date: "2017-04-10"
+    },
+    {
+        id: 3,
+        title: "Concierto Maroon 5",
+        description: "Evento Musical para ligar",
+        date: "2017-04-11"
+    },
+    {
+        id: 4,
+        title: "Concierto Rolling Stones",
+        description: "Evento Musical de leyenda",
+        date: "2017-04-12"
+    },
+    {
+        id: 5,
+        title: "Concierto Mago de Oz",
+        description: "Evento Musical de ponerse en pie y alzar el puño",
+        date: "2017-04-13"
+    }
+];
 
