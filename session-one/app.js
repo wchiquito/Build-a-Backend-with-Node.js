9e8596da1a523fcd2d012fc69ec70d752590cf8d
Hugo Nebreda <hugo.nebreda@gmail.com>
2017-03-15 19:30:12 +0100

    Create dummy event

--- session-one/app.js
+++ session-one/app.js
@@ -6,3 +6,12 @@ app.get('/', (req, res) => res.send('Hello World from get with ES6!'));
 app.post('/', (req, res) => res.send('Hello World form post with ES6!'));
 
 app.listen(3000, () =>  console.log('Example app listening on port 3000!'));
+
+
+let event = {
+    id: 1,
+    title: "Concierto Metallica",
+    description: "Evento Musical de calidad",
+    date: "2017-04-09"
+};
+
