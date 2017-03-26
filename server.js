77af1aa1d379a74f6b360a4bbade37ec66076ae7
William Chiquito <wchiquito@gmail.com>
2017-03-26 08:37:47 +0000

    M3-Refactor Improving package.json

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const app = express();
 const bodyParser = require('body-parser');
 const events = require('./routes/events');
 const appPORT = process.env.PORT || 3000;
+const appNODE_ENV = process.env.NODE_ENV || 'dev';
 
 app.use(bodyParser.json({ type: 'application/json' }));
 
-app.listen(appPORT, () => console.log('App listening on port %d on %s!', appPORT, process.env.NODE_ENV));
+app.listen(appPORT, () => console.log('App listening on port %d on %s!', appPORT, appNODE_ENV));
 
 app.get('/', (req, res) => res.send('Build a Backend with Node.js and Express.js'));
 
