3368c51ef60d971bdb9bb42dc1cb188544c57975
William Chiquito <wchiquito@gmail.com>
2017-03-26 09:43:22 +0000

    M3-Refactor Add config directory

---
+++ config/default.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const config = {
+	PORT: process.env.PORT || 3000,
+	NODE_ENV: process.env.NODE_ENV || 'dev'
+};
+
+module.exports = config;
--- server.js
+++ server.js
@@ -2,14 +2,13 @@
 
 const express = require('express');
 const app = express();
+const config = require('./config/default');
 const bodyParser = require('body-parser');
 const events = require('./routes/events');
-const appPORT = process.env.PORT || 3000;
-const appNODE_ENV = process.env.NODE_ENV || 'dev';
 
 app.use(bodyParser.json({ type: 'application/json' }));
 
-app.listen(appPORT, () => console.log('App listening on port %d on %s!', appPORT, appNODE_ENV));
+app.listen(config.PORT, () => console.log('App listening on port %d on %s!', config.PORT, config.NODE_ENV));
 
 app.get('/', (req, res) => res.send('Build a Backend with Node.js and Express.js'));
 
