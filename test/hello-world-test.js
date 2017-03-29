e9ed20b65bd2ee01f4c129b56ef270b12627798f
Hugo Nebreda <hugo.nebreda@gmail.com>
2017-03-29 19:30:02 +0200

    Configure First Test Sucessfully!

---
+++ test/hello-world-test.js
@@ -0,0 +1,6 @@
+const chai = require('chai');
+const assert = chai.assert;
+
+describe('Hello World Test!', () => {
+    it('First test', () => assert.equal( true , 1 === 1 , "Everything is alright!"));
+});
