27dfd33e54414ba45d29074842a629ec5b5d0d00
William Chiquito <wchiquito@gmail.com>
2017-03-29 18:01:00 +0000

    M4-R3 Implement a test for the endpoint that returns all the events

--- test/hello-world-test.js
+++ test/hello-world-test.js
@@ -1,6 +1,24 @@
 const chai = require('chai');
+const chaiHttp = require('chai-http');
 const assert = chai.assert;
+const server = require('../server');
+const should = chai.should();
+
+chai.use(chaiHttp);
 
 describe('Hello World Test!', () => {
     it('First test', () => assert.equal( true , 1 === 1 , "Everything is alright!"));
+});
+
+describe('/GET all Events', () => {
+    it('should GET all Events', (done) => {
+        chai.request(server)
+            .get('/events')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+                res.body.length.should.be.eql(5);
+                done();
+        })
+    });
 });
