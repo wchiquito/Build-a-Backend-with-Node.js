e3675011280e51da273b02f08d03c48dc11d2154
William Chiquito <wchiquito@gmail.com>
2017-05-05 11:33:49 +0000

    M4-Refactor Structure

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 "use strict";
 
 const express = require('express');
-const app = express();
+const server = express();
 const config = require('./config/default');
 const bodyParser = require('body-parser');
 const events = require('./routes/events');
 
-app.use(bodyParser.json({ type: 'application/json' }));
+server.use(bodyParser.json({ type: 'application/json' }));
 
-app.listen(config.PORT, () => console.log('App listening on port %d on %s!', config.PORT, config.NODE_ENV));
+server.listen(config.PORT, () => console.log('App listening on port %d on %s!', config.PORT, config.NODE_ENV));
 
-app.get('/', (req, res) => res.send('Build a Backend with Node.js and Express.js'));
+server.get('/', (req, res) => res.send('Build a Backend with Node.js and Express.js'));
 
-app.route("/events")
+server.route("/events")
   .get(events.findAll)
   .post(events.add);
 
-app.route("/events/:id")
+server.route("/events/:id")
   .get(events.findById)
   .put(events.updateById)
   .delete(events.deleteById);
 
-module.exports = app;
+module.exports = server;
